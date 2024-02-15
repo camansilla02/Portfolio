96634b3478d24fa3cfc62b497fc8bad25c2706a5
Chris <66217579+camansilla02@users.noreply.github.com>
2024-02-14 23:43:38 -0500

    Update Donations_rollback.sql

--- Scripts/SQL/Donations_rollback.sql
+++ Scripts/SQL/Donations_rollback.sql
@@ -1,7 +1,7 @@
 --rollback
- drop table zexec.zdonations;
-  drop sequence zexec.donerinfo_seq;
- drop type zexec.address_info;
- drop type zexec.address_info_ext;
- drop type zexec.phone_info;
- drop type zexec.phone_info_ext;
+ drop table some_schema.zdonations;
+  drop sequence some_schema.donerinfo_seq;
+ drop type some_schema.address_info;
+ drop type some_schema.address_info_ext;
+ drop type some_schema.phone_info;
+ drop type some_schema.phone_info_ext;
