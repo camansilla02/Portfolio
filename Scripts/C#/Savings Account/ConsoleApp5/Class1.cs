107a7e60e1146372be9eefcab77e4161e6f7a919
Chris <66217579+camansilla02@users.noreply.github.com>
2024-02-15 00:11:55 -0500

    Add files via upload

---
+++ Scripts/C#/Savings Account/ConsoleApp5/Class1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Assignment5_SAVINGS
+{
+    class SavingsAccount
+    {
+        public static double annualinterestRate = 0.04;
+        private double savingsBalance;
+        private string savingsAccountName;
+
+        //first constr
+        public SavingsAccount()
+        {
+            //by default an empty constructor sets balance to zero and string to empty
+
+        }
+        //second constructor
+        public SavingsAccount(double savingsBalance, string savingsAccountName)
+        {
+            setSavingsBalance(savingsBalance);
+            setSavingsAccountName(savingsAccountName);
+        }
+        //mutator method
+        public void setSavingsAccountName(string savingsAccountName)
+        {
+            this.savingsAccountName = savingsAccountName;
+        }
+        //accessor
+        public string getSavingsAccountName()
+        {
+            return savingsAccountName;
+        }
+        //mutatormethod
+        public void setSavingsBalance(double savingsBalance)
+        {
+            this.savingsBalance = savingsBalance;
+        }
+
+        public void CalculateMonthlyInterest()
+        {
+            savingsBalance +=((savingsBalance * annualinterestRate) / 12);
+        }
+
+        public static void setAnnualInterestRate(double aiR)
+        {
+            annualinterestRate = aiR / 100;
+        }
+
+        public void PrintSavingsAccount()
+        {
+            WriteLine("{0}       {1}         {2:C} ",  savingsAccountName, savingsBalance,string.Format("{0:0.00}",annualinterestRate));
+
+        }
+
+
+
+    }
+}
---
+++ Scripts/C#/Savings Account/ConsoleApp5/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using static System.Array;
+
+namespace Assignment5_SAVINGS
+{
+    class SavingsAccountTest
+    {
+        static void Main(string[] args)
+        {  //initialize fields and objects
+            SavingsAccount saver1 = new SavingsAccount(2000,"Saver_One");
+            SavingsAccount saver2 = new SavingsAccount(3000, "Saver_Two");
+            SavingsAccount saver3 = new SavingsAccount(); //this one is blank
+            //set annual rate and then print 1
+            SavingsAccount.setAnnualInterestRate(4);
+            saver3.setSavingsAccountName("Saver_Three");
+            saver3.setSavingsBalance(50000);
+            WriteLine("Initial savings account balances: ");
+            saver1.PrintSavingsAccount();
+            saver2.PrintSavingsAccount();
+            saver3.PrintSavingsAccount();
+            //calculate method then print again..
+            saver1.CalculateMonthlyInterest();
+            saver2.CalculateMonthlyInterest();
+            saver3.CalculateMonthlyInterest();
+            WriteLine("\nSavings account balances after calculating monthly interest at 4%: ");
+            saver1.PrintSavingsAccount();
+            saver2.PrintSavingsAccount();
+            saver3.PrintSavingsAccount();
+            //update interest rate again and print for the last time
+            SavingsAccount.setAnnualInterestRate(5);
+              WriteLine("\nSavings Account balances after calculating monthly interest at 5%: ");
+            saver1.PrintSavingsAccount();
+            saver2.PrintSavingsAccount();
+            saver3.PrintSavingsAccount();
+            WriteLine("\nPress the [Enter] key to quit.");
+            ReadLine();
+        }
+    }
+}
